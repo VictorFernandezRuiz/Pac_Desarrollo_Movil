82bff81946df9c58085cc501cc673d9ef4d3c160
VictorDeLaVera <victordelavera@gmail.com>
2021-04-08 11:59:51 +0200

    First commit

---
+++ app/src/main/java/com/example/pac_desarrollo/Activity2.kt
@@ -0,0 +1,20 @@
+package com.example.pac_desarrollo
+
+import android.R
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class Activity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(com.example.pac_desarrollo.R.layout.activity_2)
+    }
+
+
+
+}
