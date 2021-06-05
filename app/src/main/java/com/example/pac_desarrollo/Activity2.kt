858d75e4f64fbdfd22a063c51df046f80811be7a
VictorDeLaVera <victordelavera@gmail.com>
2021-06-05 12:18:55 +0200

    lo que me he encontrado lo subo para no perderlo

--- app/src/main/java/com/example/pac_desarrollo/Activity2.kt
+++ app/src/main/java/com/example/pac_desarrollo/Activity2.kt
@@ -26,6 +26,7 @@ class Activity2 : AppCompatActivity() {
     private lateinit var editNombre: EditText
     private lateinit var editPrecio: EditText
     private lateinit var fila: Cursor
+    private lateinit var buttonInsertUser: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,7 +39,27 @@ class Activity2 : AppCompatActivity() {
         editCodProd = findViewById(R.id.editCodProd)
         editNombre = findViewById(R.id.editNombre)
         editPrecio = findViewById(R.id.editPrecio)
+        buttonInsertUser = findViewById(R.id.buttonInsertUser)
 
+        private RequestQueue queue;
+
+        buttonInsertUser.setOnClickListener {
+
+
+            val queue = Volley.newRequestQueue(this)
+            val url = "http://www.google.com"
+
+            // Request a string response from the provided URL.
+            val stringRequest = StringRequest(Request.Method.GET, url,
+                    Response.Listener<String> { response ->
+                        // Display the first 500 characters of the response string.
+                        textView.text = "Response is: ${response.substring(0, 500)}"
+                    },
+                    Response.ErrorListener { textView.text = "That didn't work!" })
+
+            // Add the request to the RequestQueue.
+            queue.add(stringRequest)
+        }
 
 
          buttonInsertarDatos.setOnClickListener {
